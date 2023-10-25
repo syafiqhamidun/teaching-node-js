94d2ee744dc6d3c2b268bdcc8db3a430e8a40fda
Syafiq Hamidun <syafiqhamidun10@gmail.com>
2023-10-25 18:55:25 +0800

    todo.controller adjustment

--- 5-model/personal/todo-app-node/controller/todo.controller.js
+++ 5-model/personal/todo-app-node/controller/todo.controller.js
@@ -1,3 +1,9 @@
-export const getAllTodo = (req,res) => {
+import { query } from "../database/index.js"
+
+
+
+export const getAllTodo = async(req,res) => {
+    const data = await query('SELECT * FROM todos');
+    console.log(data.rows);
     res.render("pages/index");
-};
+} 
